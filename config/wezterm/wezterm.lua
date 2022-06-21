e7f67e08a40c9b0ac6725aade3171c86a04ba3cf
meleehao <meleehao@outlook.com>
2022-06-21 19:33:31 +0800

    new file:   config/alacritty/alacritty.yml 	new file:   config/wezterm/wezterm.lua

---
+++ config/wezterm/wezterm.lua
@@ -0,0 +1,28 @@
+local wezterm = require 'wezterm';
+
+
+return {
+
+  font = wezterm.font("Fira Code"),
+
+  -- You can specify some parameters to influence the font selection;
+
+  -- for example, this selects a Bold, Italic font variant.
+
+  default_prog = {"C:/Program Files/PowerShell/7/pwsh.exe", "-l"},
+  font = wezterm.font("Sarasa Term TC Semibold", {weight="Bold", italic=true}),
+
+  font_size = 12,
+
+  color_scheme = "iceberg-dark",
+
+  enable_tab_bar = false,
+
+  initial_cols = 110,
+
+  initial_rows = 30,
+
+--  wayland_enable = true,
+
+}
+
